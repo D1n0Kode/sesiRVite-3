ec1e0a57af9c5c88f9df73556c2208bb362ca3a4
D1n0Kode <kevinsitarz2022@gmail.com>
2024-03-25 11:44:02 -0300

    subindo pro rep o react vite

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Card from "./componentes/Card";
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Card/>
+    </>
+  )
+}
+
+export default App
---
+++ src/componentes/Card.jsx
@@ -0,0 +1,17 @@
+import Foto01 from "../assets/img/foto01.png"
+
+
+export default function Card(){
+    return(
+        <div>
+        <h2></h2>
+        <img src={Foto01} alt="Paisagem" width={200} height="auto"/>
+        <p>sdadadada</p>
+        
+        </div>
+    
+    
+    )
+
+
+}
